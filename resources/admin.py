4d4a4f5125d988ecaa6f3c98d63511754dbae7fa
LaameriSayf <saif.laameri@esprit.tn>
2025-10-29 22:09:43 +0100

    commit architecture

--- resources/admin.py
+++
@@ -1,9 +0,0 @@
-#from django.contrib import admin
-#from .models import Resource
-
-#@admin.register(Resource)
-#class ResourceAdmin(admin.ModelAdmin):
-#    list_display = ('title', 'resource_type', 'uploaded_at', 'processed')
- #   list_filter = ('resource_type', 'processed')
-  #  search_fields = ('title', 'tags')
-    #readonly_fields = ('uploaded_at', 'summary', 'formulas', 'images_extracted')
